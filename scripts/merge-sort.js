0ee3dfab69fc6a38cdb7d705e733897b01481cbb
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2017-10-18 15:30:08 -0600

    testing is solid now

--- scripts/merge-sort.js
+++ scripts/merge-sort.js
@@ -1,26 +1,22 @@
-
 function mergeSort(array){
     //base case
     if (array.length <=1) {
       // debugger
         return array;
     }
-    const midPoint = Math.floor(array.lenght /2);
+    const midpoint = Math.floor(array.length /2);
     //midpoint exclusive
-    const a = array.slice(0, midPoint);
+    const a = array.slice(0, midpoint);
     const b = array.slice(midpoint, array.length);
-    console.log(a);
-    console.log(b);
     return merge(mergeSort(a), mergeSort(b));
 }
 function merge(a, b){
     let result=[];
-    console.log('merge happening')
     while (a.length && b.length) {
       if (a[0] <= b[0]) {
         result.push (a.shift())
       } else {
-        result.push (b.shift)
+        result.push (b.shift())
       }
     }
     while (a.length) {
--- tests/quick-sort-test.js
+++ tests/quick-sort-test.js
@@ -2,17 +2,17 @@ import { assert } from 'chai';
 const quickSort = require('../scripts/quick-sort.js');
 
 describe('quick sort functionality', () => {
+  it('should be a function', () => {
+    assert.isFunction(quickSort);
+  })
   it('should sort small array', () => {
     const array = [5, 3, 8, 23, 9, 34, 2]
     assert.deepEqual( quickSort(array), [2, 3, 5, 8, 9, 23, 34]);
   })
-  it.skip('should sort negative numbers', () => {
-    //repeat for negative #'s'
+  it('should sort negative numbers', () => {
+    const array = [-5, -3, -8, -23, -9, -34, -2]
+    assert.deepEqual( quickSort(array), [-34, -23, -9, -8, -5, -3, -2]);
   })
-  // it.skip('should sort positive numbers', () => {
-  //
-  //   assert(true).to.equal(true);
-  // })
   it('should sort an odd array of charaters', () => {
     const array = ['p', 'g', 'e', 'a']
     assert.deepEqual( quickSort(array), ['a', 'e', 'g', 'p']);
@@ -21,6 +21,29 @@ describe('quick sort functionality', () => {
     let array = [];
     for (let i = 0; i < 50000; i++) {
       let randNum = Math.floor(Math.random() * 100000);
+      array.push(randNum);
+    }
+    let sortedArray = quickSort(array);
+    for (let i = 0; i < sortedArray.length - 1; i++) {
+      assert.equal(sortedArray[i] <= sortedArray[i+1], true)
+    }
+  })
+  it('should sort an array of 500,000 numbers', () => {
+    let array = [];
+    for (let i = 0; i < 500000; i++) {
+      let randNum = Math.floor(Math.random() * 1000000);
+      array.push(randNum);
+    }
+    let sortedArray = quickSort(array);
+    for (let i = 0; i < sortedArray.length - 1; i++) {
+      assert.equal(sortedArray[i] <= sortedArray[i+1], true)
+    }
+  })
+  it('should sort an array of 1,000,000 numbers', () => {
+    let array = [];
+    for (let i = 0; i < 1000000; i++) {
+      let randNum = Math.floor(Math.random() * 10000000);
+      array.push(randNum);
     }
     let sortedArray = quickSort(array);
     for (let i = 0; i < sortedArray.length - 1; i++) {
@@ -30,10 +53,10 @@ describe('quick sort functionality', () => {
 })
 
 
-//negative numbers
-//positive numbers
-//odd array of mixed characters
-//sort large numbers
+// negative numbers
+// positive numbers
+// array of mixed characters
+// sort large numbers
 // 50,000
 // 500,000
 // 1,000,000
